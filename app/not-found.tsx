ac452d65b9fab645ca7c6ae825170c3c03b43653
thecoachmanuel <manueloliver2908@gmail.com>
2026-01-13 13:10:52 +0100

    chore(app): force dynamic node runtime for not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import { prisma } from '@lib/db'
+export const runtime = 'nodejs'
+export const dynamic = 'force-dynamic'
 
 export default async function NotFound() {
   const settings = await prisma.siteSetting.findFirst()
