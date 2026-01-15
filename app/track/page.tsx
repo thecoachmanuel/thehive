8d1157f5a36599d48f462b43db53e1d9f0906e7e
thecoachmanuel <manueloliver2908@gmail.com>
2026-01-15 01:01:07 +0100

    track update

--- app/track/page.tsx
+++ app/track/page.tsx
@@ -1,17 +1,16 @@
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import TrackOrderClient from '@components/TrackOrderClient'
-import { prisma } from '@lib/db'
+
 export const runtime = 'nodejs'
 export const dynamic = 'force-dynamic'
 
-export default async function TrackOrder() {
-  const settings = await prisma.siteSetting.findFirst()
-  return (
-    <div>
-      <Header name={settings?.businessName} logoUrl={settings?.logoUrl ?? undefined} />
-      <TrackOrderClient />
-      <Footer />
-    </div>
-  )
+export default function TrackOrder() {
+	return (
+		<div>
+			<Header />
+			<TrackOrderClient />
+			<Footer />
+		</div>
+	)
 }
