c57b68e7486a19c3cce3d8e85a9062140f5cf7d2
thecoachmanuel <manueloliver2908@gmail.com>
2026-01-15 22:37:41 +0100

    cd

--- next.config.js
+++ next.config.js
@@ -11,7 +11,8 @@ const nextConfig = {
 	images: {
 		remotePatterns: [
 			{ protocol: 'https', hostname: 'images.unsplash.com' },
-			{ protocol: 'https', hostname: 'images.pexels.com' }
+			{ protocol: 'https', hostname: 'images.pexels.com' },
+			{ protocol: 'https', hostname: 'res.cloudinary.com' }
 		]
 	}
 }
